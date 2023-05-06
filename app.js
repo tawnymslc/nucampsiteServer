f0bb8a5ace894d9f5045011b791a0e22ef566ea6
Tawny Mathi <tawnymslc@gmail.com>
2023-05-05 19:37:07 -0600

    Express REST API with MongoDB and Mongoose Part 1

--- app.js
+++ app.js
@@ -10,6 +10,20 @@ var campsiteRouter = require('./routes/campsiteRouter');
 var promotionRouter = require('./routes/promotionRouter');
 var partnerRouter = require('./routes/partnerRouter');
 
+const mongoose = require('mongoose');
+
+const url = 'mongodb://localhost:27017/nucampsite';
+const connect = mongoose.connect(url, {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+});
+
+connect.then(() => console.log('Connected correctly to server'), 
+    err => console.log(err)
+);
+
 var app = express();
 
 // view engine setup
